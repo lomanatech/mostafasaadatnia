9b60d6beb86b7de006730eaddfb7bfb2dc560bcb
MostafaSaadatnia <40535745+MostafaSaadatnia@users.noreply.github.com>
2024-01-11 19:55:16 +0330

    add style

--- src/Person/Person.tsx
+++ src/Person/Person.tsx
@@ -1,8 +1,9 @@
 import React from 'react'
+import './Person.css';
 
 const person = (props: any) => {
     return (
-        <div>
+        <div className='Person'>
             <p onClick={props.click}>I'm {props.name} and I'm {props.age} years old!</p>
             <p>{props.children}</p>
             <input type='text' onChange={props.changed} />
