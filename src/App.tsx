fad3603e1512190de69635ec5b4657d8f3bcb4b0
MostafaSaadatnia <40535745+MostafaSaadatnia@users.noreply.github.com>
2024-01-11 17:05:00 +0330

    use state for dynamic values

--- src/App.tsx
+++ src/App.tsx
@@ -1,16 +1,23 @@
-import React from 'react';
+import React, { useState } from 'react';
 import logo from './logo.svg';
 import './App.css';
 import Person from './Person/Person'
 
 function App() {
+  const [state, setState] = useState({
+    persons: [
+      { name: "Mostafa", age: 31 },
+      { name: "Firoozeh", age: 32 },
+      { name: "Maysam", age: 22 },
+    ]
+  });
   return (
     <div className="App">
       <h1>Hi, It's a React App!</h1>
       <p>It works perfectly indeed.</p>
-      <Person name="Mostafa" age="31" />
-      <Person name="Firoozeh" age="32">I like React!</Person>
-      <Person name="Maysam" age="22" />
+      <Person name={state.persons[0].name} age={state.persons[0].age} />
+      <Person name={state.persons[1].name} age={state.persons[1].age}>I like React!</Person>
+      <Person name={state.persons[2].name} age={state.persons[2].age} />
     </div>
   );
 }
