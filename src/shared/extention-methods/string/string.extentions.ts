5cb3208bf7bae8c173211c4af94e3f22b117a65e
m.saadatnia <m.saadatnia@M-SAADATNIA>
2024-01-18 15:30:42 +0330

    add some perfect extension methods into the string.extensions.ts file

--- src/shared/extention-methods/string/string.extentions.ts
+++ src/shared/extention-methods/string/string.extentions.ts
@@ -1,3 +1,55 @@
+
+
+interface String {
+    toEmptyIfNull(): string;
+    padZero(length: number): string;
+    startsWith(searchTerm: string, ignoreCase: boolean): boolean;
+    endsWith(searchTerm: string, ignoreCase: boolean): boolean;
+    contains(searchTerm: string, ignoreCase: boolean): boolean;
+    equals(value: string, ignoreCase: boolean): boolean;
+    isEmpty(): boolean;
+}
+
 String.prototype.toEmptyIfNull = function (this: string): string {
     return this === null ? '' : this;
 };
+
+String.prototype.padZero = function (length: number): string {
+    let s = this;
+    while (s.length < length) {
+        s = '0' + s;
+    }
+    return s as any;
+};
+
+String.prototype.contains = function (searchTerm: string, ignoreCase: boolean = false): boolean {
+    if (ignoreCase) {
+        return this.toLowerCase().includes(searchTerm.toLowerCase());
+    }
+    return this.includes(searchTerm);
+};
+
+String.prototype.equals = function (value: string, ignoreCase: boolean = false): boolean {
+    if (ignoreCase) {
+        return this.toLowerCase() === value.toLowerCase();
+    }
+    return this === value;
+};
+
+String.prototype.isEmpty = function (): boolean {
+    return this.trim().length < 1;
+};
+
+// String.prototype.startsWith = function(searchTerm: string, ignoreCase: boolean = false): boolean {
+//     if (ignoreCase) {
+//         return this.toLowerCase().startsWith(searchTerm.toLowerCase());
+//     }
+//     return this.startsWith(searchTerm);
+// };
+
+// String.prototype.endsWith = function(searchTerm: string, ignoreCase: boolean = false): boolean {
+//     if (ignoreCase) {
+//         return this.toLowerCase().endsWith(searchTerm.toLowerCase());
+//     }
+//     return this.endsWith(searchTerm);
+// };
